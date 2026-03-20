eb37ebc0d51b9af06a396195e9436cefb5a20ab8
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-20 23:57:11 +0500

    feat(server): add tracking route and enable JSON parsing middleware

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import couriersRoute from './routes/couriersRoute.js'
+import trackingRoute from './routes/trackingRoute.js'
+
 const app = express();
 
-const PORT = process.env.PORT;
+app.use(express.json());
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
 }))
 app.use('/api/v1/couriers',couriersRoute)
+app.use('/api/v1/trackings',trackingRoute);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`visit http://localhost:3200`);
+    console.log(`visit http://localhost:${PORT}`);
 })
