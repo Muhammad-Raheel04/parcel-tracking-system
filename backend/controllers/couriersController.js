77bacf75f75656e3664b8a6012fd391f4434f86e
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-20 21:58:54 +0500

    feat: add API endpoint to fetch all couriers from TrackingMore

---
+++ backend/controllers/couriersController.js
@@ -0,0 +1,24 @@
+export const getCouriers=async(req,res)=>{
+     try {
+        const response = await fetch(`https://api.trackingmore.com/v4/couriers/all`, {
+            headers: {
+                'Tracking-Api-Key': process.env.TRACKING_API_KEY,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+
+        const data=await response.json();
+        console.log(data);
+        return res.status(200).json({
+            success:true,
+            message:"successfully fetched list of couriers",
+            data:data.data,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
