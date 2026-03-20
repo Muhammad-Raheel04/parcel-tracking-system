beb81686669a7ca7c2318722a4cd449eabba9a8b
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-20 22:01:16 +0500

    feat(server): setup Express app with CORS and /api/v1/couriers route

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import couriersRoute from './routes/couriersRoute.js'
+const app = express();
 
-const app=express();
-
-const PORT=process.env.PORT;
-app.use('/',(req,res)=>{
-    return res.status(200).json({
-        success:"true",
-        message:"server is running",
-    })
-})
-app.listen(PORT,()=>{
+const PORT = process.env.PORT;
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}))
+app.use('/api/v1/couriers',couriersRoute)
+app.listen(PORT, () => {
     console.log(`visit http://localhost:3200`);
 })
